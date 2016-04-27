d1c4cf2d159ee7abe467698edcf1c75c9880069a
unknown <smj1007@AT230XWKS-035W.srunet.sruad.edu>
2016-04-27 14:59:46 -0400

    new commit

---
+++ Assets/Scripts/ThrowController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ThrowController : MonoBehaviour {
+	
+	private BallController ballScript;
+	public GameObject prefab;
+	private GameObject parentBone;
+	private GameObject ball;
+	public int numBalls;
+	//public Animator animator;
+	public Text ballsRemaining;
+
+
+	// Use this for initialization
+	void Start () {
+
+		numBalls = 30;
+        //animator = GetComponent<Animator> ();
+        parentBone = GameObject.Find("parentBone");
+		ballsRemaining = GameObject.Find("BallCount").GetComponent<Text> ();
+		ball = Instantiate (prefab, parentBone.transform.position, parentBone.transform.rotation) as GameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//ballsRemaining.text = "Balls Remaining: " + numBalls.ToString();
+
+		if (Input.GetMouseButtonDown (0)) {
+			ThrowBall ();
+
+		}
+
+	}
+
+	void ThrowBall(){
+
+		ballScript = ball.GetComponent<BallController> ();
+		ballScript.Release ();
+
+		if (numBalls > 0) {
+			Invoke ("makeNewBall", 1.0f);
+			numBalls--;
+		}
+
+		//Debug.Log ("Throw Ball num balls is " + numBalls);
+
+	}
+
+	public void makeNewBall(){
+		ball = Instantiate (prefab, parentBone.transform.position, parentBone.transform.rotation) as GameObject;
+
+	}
+		
+
+	public int getNumBalls(){
+		return this.numBalls;
+	}
+
+
+
+}
